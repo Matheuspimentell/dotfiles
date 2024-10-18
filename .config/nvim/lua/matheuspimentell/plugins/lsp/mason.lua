da74d042d4702c19a34c073b74c872d4f446920b
Matheus Pimentel <matheusp.eng@gmail.com>
2024-10-18 17:19:17 -0300

    Finished lsp setup on nvim

---
+++ .config/nvim/lua/matheuspimentell/plugins/lsp/mason.lua
@@ -0,0 +1,31 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "gopls", --  Go
+        "lua_ls", -- Lua
+        "rust_analyzer", -- Rust
+        "sqls", --  SQL
+        "clangd", -- C
+        "pyright", -- Python
+      },
+    })
+  end,
+}
